0d9e687c488e4ac6b5d61cae295c1facd654d365
bahri444 <bahrysaipul266@gmail.com>
2024-03-07 20:32:14 +0700

    uncomment auth krs

--- app/Http/Controllers/KrsMahasiswaController.php
+++ app/Http/Controllers/KrsMahasiswaController.php
@@ -7,10 +7,10 @@ use Illuminate\Http\Request;
 
 class KrsMahasiswaController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth:api');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
     public function GetKrsMahasiswa()
     {
         $data_krs = KrsMahasiswa::with('mahasiswa', 'nilai', 'matakuliah', 'kelas', 'pengajar.dosen', 'prodi')->get();
