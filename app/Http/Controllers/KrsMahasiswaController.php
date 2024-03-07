f1c0379e3743b37b9958b9e4bee2ca5daec8d92a
bahri444 <bahrysaipul266@gmail.com>
2024-03-07 20:30:09 +0700

    manage response krs mahasiswa

--- app/Http/Controllers/KrsMahasiswaController.php
+++ app/Http/Controllers/KrsMahasiswaController.php
@@ -7,16 +7,26 @@ use Illuminate\Http\Request;
 
 class KrsMahasiswaController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:api');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:api');
+    // }
     public function GetKrsMahasiswa()
     {
         $data_krs = KrsMahasiswa::with('mahasiswa', 'nilai', 'matakuliah', 'kelas', 'pengajar.dosen', 'prodi')->get();
-        // dd($data_krs);
+        $data = [];
+
+        foreach ($data_krs as $key) {
+            array_push($data, [
+                'nipd' => $key->mahasiswa->nipd,
+                'nama_mahasiswa' => $key->mahasiswa->nm_pd,
+                'nama_mk' => $key->matakuliah->nama_mk,
+                'jumlah_sks' => $key->matakuliah->sks_tatap_muka,
+                'nama_dosen' => $key->pengajar->dosen->nama_dosen,
+            ]);
+        }
         return response()->json([
-            'data' => $data_krs,
+            'data' => $data,
             'success' => 'data krs mahasiswa pada semester aktif'
         ], 200);
     }
