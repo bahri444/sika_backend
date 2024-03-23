5023de3685e338fbfa4c7877fb5f99754715c24e
bahri444 <bahrysaipul266@gmail.com>
2024-03-23 23:25:23 +0700

    update aktifitas mahasiswa controller

--- app/Http/Controllers/AktifitasMahasiswaController.php
+++ app/Http/Controllers/AktifitasMahasiswaController.php
@@ -2,17 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KrsMahasiswa;
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AktifitasMahasiswaController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth:api');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
     public function GetAktifitasMahasiswa()
     {
-        $data_aktifitas = "data mahasiswa yang bersangkutan";
-        return response()->json(['data' => $data_aktifitas, 'success' => 'data aktifitas mahasiswa'], 200);
+        $data_semester = Mahasiswa::with('krs_mahasiswa', 'krs_mahasiswa.data_semester')->where('nipd', Auth::user()->nim)->get();
+
+        $data_krs = KrsMahasiswa::with('matakuliah')->where('nipd', Auth::user()->nim)->get();
+        $aktifitas_semester = $data_krs->sum('matakuliah.sks_tatap_muka');
+
+        $data = [];
+        foreach ($data_semester as $val) {
+            array_push($data, [
+                'nama_semester' => $val->krs_mahasiswa->data_semester->nama_semester,
+                'sks' => $aktifitas_semester,
+            ]);
+        }
+        return response()->json(['data' => $data, 'success' => 'data aktifitas mahasiswa'], 200);
     }
 }
