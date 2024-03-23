b44fa46492dba8d84263f1d039db64ea6539cfe1
bahri444 <bahrysaipul266@gmail.com>
2024-03-23 22:41:54 +0700

    update transkrip nilai controller

--- app/Http/Controllers/TranskripNilaiController.php
+++ app/Http/Controllers/TranskripNilaiController.php
@@ -2,17 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mahasiswa;
+use App\Models\NilaiMahasiswa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TranskripNilaiController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth:api');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
     public function GetTranskripNilai()
     {
-        $data_transkrip_nilai = "data trasnkrip nilai mahasiswa yang bersangkutan";
-        return response()->json(['data' => $data_transkrip_nilai, 'success' => 'data trasnkrip nilai mahasiswa'], 200);
+        $data_semester = Mahasiswa::with('krs_mahasiswa', 'krs_mahasiswa.data_semester')->where('nipd', Auth::user()->nim)->get();
+
+        $count_data_nilai = NilaiMahasiswa::where('nipd', Auth::user()->nim)->get();
+
+        $ipk = $count_data_nilai->sum('nilai_index') / $count_data_nilai->count();
+
+        $data = [];
+        foreach ($data_semester as $val) {
+            array_push($data, [
+                'nama_semester' => $val->krs_mahasiswa->data_semester->nama_semester,
+                'ipk_semester' => $ipk,
+            ]);
+        }
+        return response()->json(['data' => $data, 'success' => 'data trasnkrip nilai mahasiswa'], 200);
     }
 }
